0eb41d0cb86c7046012fb659a0060867481fd8c2
Jacob Reimers <jacob@reimers.dk>
2026-01-07 09:34:06 +0100

    Add signature verification

--- src/opencertserver.ca.utils/CertificateRevocationList.cs
+++ src/opencertserver.ca.utils/CertificateRevocationList.cs
@@ -6,19 +6,35 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace OpenCertServer.Ca.Utils;
 
+/// <summary>
+/// Defines a Certificate Revocation List (CRL).
+/// </summary>
 public class CertificateRevocationList
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CertificateRevocationList"/> class.
+    /// </summary>
+    /// <param name="version">The CRL format version.</param>
+    /// <param name="crlNumber">The CRL number.</param>
+    /// <param name="signature">The CRL signature.</param>
+    /// <param name="signatureAlgorithm">The signature algorithm used.</param>
+    /// <param name="issuer">The distinguished name of the CRL issuer.</param>
+    /// <param name="thisUpdate">The <see cref="DateTimeOffset"/> when the CRL was issued.</param>
+    /// <param name="nextUpdate">The <see cref="DateTimeOffset"/> when the next CRL will be issued.</param>
+    /// <param name="revokedCertificates">The <see cref="List{T}"/> of revoked <see cref="RevokedCertificates"/>.</param>
     public CertificateRevocationList(
         CrlVersion version,
         BigInteger crlNumber,
-        string signatureAlgorithm,
+        byte[] signature,
+        HashAlgorithmName signatureAlgorithm,
         X500DistinguishedName issuer,
         DateTimeOffset thisUpdate,
         DateTimeOffset? nextUpdate,
-        IEnumerable<RevokedCertificate> revokedCertificates)
+        params IEnumerable<RevokedCertificate> revokedCertificates)
     {
         Version = version;
         CrlNumber = crlNumber;
+        Signature = signature;
         SignatureAlgorithm = signatureAlgorithm;
         Issuer = issuer;
         ThisUpdate = thisUpdate;
@@ -26,50 +42,106 @@ public class CertificateRevocationList
         RevokedCertificates = revokedCertificates.ToList().AsReadOnly();
     }
 
+    /// <summary>
+    /// Gets the list of revoked certificates.
+    /// </summary>
     public ReadOnlyCollection<RevokedCertificate> RevokedCertificates { get; set; }
+
+    /// <summary>
+    /// Gets the CRL version.
+    /// </summary>
     public CrlVersion Version { get; }
+
+    /// <summary>
+    /// Gets the CRL number.
+    /// </summary>
     public BigInteger CrlNumber { get; }
-    public string SignatureAlgorithm { get; }
+
+    /// <summary>
+    /// Gets the CRL signature.
+    /// </summary>
+    public byte[] Signature { get; }
+
+    /// <summary>
+    /// Gets the signature algorithm used.
+    /// </summary>
+    public HashAlgorithmName SignatureAlgorithm { get; }
+
+    /// <summary>
+    /// Gets the issuer distinguished name.
+    /// </summary>
     public X500DistinguishedName Issuer { get; }
+
+    /// <summary>
+    /// Gets the <see cref="DateTimeOffset"/> for this update.
+    /// </summary>
     public DateTimeOffset ThisUpdate { get; }
+
+    /// <summary>
+    /// Gets the <see cref="DateTimeOffset"/> for the next update.
+    /// </summary>
     public DateTimeOffset? NextUpdate { get; }
 
+    /// <summary>
+    /// Defines the CRL version.
+    /// </summary>
     public enum CrlVersion
     {
         V1 = 0,
-        V2 = 1
+        V2 = 1,
+        V3 = 2
     }
 
-    public static CertificateRevocationList Load(ReadOnlySpan<byte> currentCrl)
+    /// <summary>
+    /// Reads and verifies a DER-encoded CRL using the issuer's public key.
+    /// </summary>
+    /// <param name="crl">The DER encoded certificate revocation list.</param>
+    /// <param name="issuerPublicKey">The public key of the issuer.</param>
+    /// <returns>A populated instance of a <see cref="CertificateRevocationList"/>.</returns>
+    /// <exception cref="CryptographicException">Raised if the signature cannot be verified or the content is malformed.</exception>
+    public static CertificateRevocationList Load(ReadOnlyMemory<byte> crl, AsymmetricAlgorithm issuerPublicKey)
     {
+        // CRL ::= SEQUENCE {
+        //     tbsCertList             TBSCertList,
+        //     signatureAlgorithm      AlgorithmIdentifier,
+        //     signatureValue          BIT STRING
+        // }
+
+        var tbsSpan = ReadSignedContent(crl);
+
         BigInteger crlNumber = 0;
 
-        try
+        var reader = new AsnReader(crl, AsnEncodingRules.DER);
+        var certificateList = reader.ReadSequence();
+        var tbsCertList = certificateList.ReadSequence();
+        var algoIdentifier = certificateList.ReadSequence();
+        reader.ThrowIfNotEmpty();
+        var signature = certificateList.ReadBitString(out _, Asn1Tag.PrimitiveBitString);
+
+        var signatureAlgo = GetHashAlgorithmFromOid(algoIdentifier.ReadObjectIdentifier(Asn1Tag.ObjectIdentifier));
+
+        if (!VerifySignature(tbsSpan, signature, issuerPublicKey, signatureAlgo))
         {
-            var reader = new AsnValueReader(currentCrl, AsnEncodingRules.DER);
+            throw new CryptographicException("CRL signature verification failed.");
+        }
 
-            var certificateList = reader.ReadSequence();
-            var tbsCertList = certificateList.ReadSequence();
+        var version = ReadVersion(tbsCertList);
 
-            var version = ReadVersion(ref tbsCertList);
+        // Discard algorithm identifier as it is supposed to match the outer one
+        _ = tbsCertList.ReadSequence();
 
-            var algoAsn = ReadAlgorithmIdentifierAsn(ref tbsCertList);
-            var distinguishedName = ReadDistinguishedName(ref tbsCertList);
+        var distinguishedName = ReadDistinguishedName(ref tbsCertList);
 
-            // thisUpdate
-            var thisUpdate = ReadX509Time(ref tbsCertList);
+        // thisUpdate
+        var thisUpdate = ReadX509Time(ref tbsCertList);
 
-            // nextUpdate
-            var nextUpdate = ReadX509TimeOpt(ref tbsCertList);
+        // nextUpdate
+        var nextUpdate = ReadX509TimeOpt(ref tbsCertList);
 
-            AsnValueReader revokedCertificates = default;
+        // revokedCertificates
+        var list = ReadRevokedCertificates(ref tbsCertList, version);
 
-            if (tbsCertList.HasData && tbsCertList.PeekTag().HasSameClassAndValue(Asn1Tag.Sequence))
-            {
-                revokedCertificates = tbsCertList.ReadSequence();
-            }
-
-            // TODO: Handle extensions
+        // TODO: Handle extensions
 //
 //            if (version > 0 && tbsCertList.HasData)
 //            {
@@ -116,24 +188,98 @@ public class CertificateRevocationList
 //
 //            tbsCertList.ThrowIfNotEmpty();
 
-            var list = ReadRevokedCertificates(revokedCertificates, version);
-
-            return new CertificateRevocationList(
-                (CrlVersion)version,
-                crlNumber,
-                algoAsn.Algorithm,
-                distinguishedName,
-                thisUpdate,
-                nextUpdate,
-                list);
-        }
-        catch (AsnContentException e)
-        {
-            throw new CryptographicException("Invalid DER encoding.", e);
-        }
+        return new CertificateRevocationList(
+            (CrlVersion)version,
+            crlNumber,
+            signature,
+            signatureAlgo,
+            distinguishedName,
+            thisUpdate,
+            nextUpdate,
+            list);
     }
 
-    private static int ReadVersion(ref AsnValueReader tbsCertList)
+    /// <summary>
+    /// Verifies the signature of a DER-encoded CRL using the issuer's public key.
+    /// </summary>
+    /// <param name="crl">The DER encoded CRL.</param>
+    /// <param name="issuerPublicKey">The public key of the CRL issuer.</param>
+    /// <returns><c>True</c> if the signature is verified, otherwise <c>False</c></returns>
+    /// <exception cref="CryptographicException">Thrown if the content is malformed.</exception>
+    public static bool VerifyCrlSignature(ReadOnlyMemory<byte> crl, AsymmetricAlgorithm issuerPublicKey)
+    {
+        var tbsSpan = ReadSignedContent(crl);
+
+        // Now read the outer SEQUENCE again to get to signatureAlgorithm and signatureValue
+        var reader = new AsnReader(crl, AsnEncodingRules.DER);
+        var certificateList = reader.ReadSequence(Asn1Tag.Sequence);
+        reader.ThrowIfNotEmpty(); // Ensure we consumed only tbsCertList so far? No — continue
+
+        // 1. Read tbsCertList (first SEQUENCE)
+        _ = certificateList.ReadSequence(Asn1Tag.Sequence);
+
+        // 2. Read signatureAlgorithm
+        var signatureAlgorithm = certificateList.ReadSequence(Asn1Tag.Sequence);
+        var oid = signatureAlgorithm.ReadObjectIdentifier(Asn1Tag.ObjectIdentifier);
+
+        // Map OID to HashAlgorithmName
+        var hashAlgorithm = GetHashAlgorithmFromOid(oid);
+
+        // 3. Read signatureValue (BIT STRING)
+        var signatureValue = certificateList.ReadBitString(out var unusedBits, Asn1Tag.ConstructedBitString);
+        if (unusedBits != 0)
+            throw new CryptographicException("Signature BIT STRING has unused bits != 0");
+        certificateList.ThrowIfNotEmpty();
+
+        // Remove leading zero if present (some implementations add it)
+        var signature = signatureValue.Length > 0 && signatureValue[0] == 0
+            ? signatureValue.AsSpan(1).ToArray()
+            : signatureValue;
+        return VerifySignature(tbsSpan, signature, issuerPublicKey, hashAlgorithm);
+    }
+
+    private static ReadOnlySpan<byte> ReadSignedContent(ReadOnlyMemory<byte> crlDer)
+    {
+        // Read outer SEQUENCE, get content span (tbs + alg + sig)
+        AsnDecoder.ReadSequence(crlDer.Span, AsnEncodingRules.DER, out var contentOffset, out var contentLength,
+            out _, Asn1Tag.Sequence);
+        var contentSpan = crlDer.Span.Slice(contentOffset, contentLength);
+
+        // Read tbsCertList SEQUENCE, calculate its full encoded length
+        AsnDecoder.ReadSequence(contentSpan, AsnEncodingRules.DER, out var tbsOffset, out var tbsLength, out _,
+            Asn1Tag.Sequence);
+        var tbsSpan = contentSpan[..(tbsOffset + tbsLength)];
+        return tbsSpan;
+    }
+
+    private static bool VerifySignature(ReadOnlySpan<byte> data, ReadOnlySpan<byte> signature, AsymmetricAlgorithm publicKey, HashAlgorithmName hashAlgorithm)
+    {
+        return publicKey switch
+        {
+            RSA rsa => rsa.VerifyData(data.ToArray(), signature.ToArray(), hashAlgorithm, RSASignaturePadding.Pss),
+            ECDsa ecdsa => ecdsa.VerifyData(data.ToArray(), signature.ToArray(), hashAlgorithm, DSASignatureFormat.Rfc3279DerSequence),
+            _ => throw new NotSupportedException($"Public key type {publicKey.GetType()} not supported.")
+        };
+    }
+
+    private static HashAlgorithmName GetHashAlgorithmFromOid(string oid)
+    {
+        return oid switch
+        {
+            "1.2.840.113549.1.1.10"=> HashAlgorithmName.SHA256, // rsassaPss
+            "1.2.840.113549.1.1.11" => HashAlgorithmName.SHA256, // sha256WithRSAEncryption
+            "1.2.840.113549.1.1.12" => HashAlgorithmName.SHA384,
+            "1.2.840.113549.1.1.13" => HashAlgorithmName.SHA512,
+            "1.2.840.113549.1.1.5" => HashAlgorithmName.SHA1, // sha1WithRSAEncryption
+            "1.2.840.10045.4.3.2" => HashAlgorithmName.SHA256, // ecdsa-with-SHA256
+            "1.2.840.10045.4.3.3" => HashAlgorithmName.SHA384,
+            "1.2.840.10045.4.3.4" => HashAlgorithmName.SHA512,
+            "1.2.840.10045.4.1" => HashAlgorithmName.SHA1, // ecdsa-with-SHA1
+            _ => throw new CryptographicException($"Unsupported signature algorithm OID: {oid}")
+        };
+    }
+
+    private static int ReadVersion(AsnReader tbsCertList)
     {
         var version = 0;
 
@@ -153,13 +299,7 @@ public class CertificateRevocationList
         return version;
     }
 
-    private static AlgorithmIdentifierAsn ReadAlgorithmIdentifierAsn(ref AsnValueReader tbsCertList)
-    {
-        AlgorithmIdentifierAsn.Decode(ref tbsCertList, ReadOnlyMemory<byte>.Empty, out var algoAsn);
-        return algoAsn;
-    }
-
-    private static X500DistinguishedName ReadDistinguishedName(ref AsnValueReader tbsCertList)
+    private static X500DistinguishedName ReadDistinguishedName(ref AsnReader tbsCertList)
     {
         // X500DN
         var distinguishedName = new X500DistinguishedNameBuilder();
@@ -176,9 +316,15 @@ public class CertificateRevocationList
         return distinguishedName.Build();
     }
 
-    private static List<RevokedCertificate> ReadRevokedCertificates(AsnValueReader revokedCertificates, int version)
+    private static List<RevokedCertificate> ReadRevokedCertificates(ref AsnReader tbsCertList, int version)
     {
         List<RevokedCertificate> list = [];
+        if (!tbsCertList.HasData || !tbsCertList.PeekTag().HasSameClassAndValue(Asn1Tag.Sequence))
+        {
+            return list;
+        }
+        var revokedCertificates = tbsCertList.ReadSequence();
+
         while (revokedCertificates.HasData)
         {
             var valueReader = revokedCertificates.ReadSequence();
@@ -203,14 +349,14 @@ public class CertificateRevocationList
         return list;
     }
 
-    private static DateTimeOffset ReadX509Time(ref AsnValueReader reader)
+    private static DateTimeOffset ReadX509Time(ref AsnReader reader)
     {
         return reader.PeekTag().HasSameClassAndValue(Asn1Tag.UtcTime)
             ? reader.ReadUtcTime()
             : reader.ReadGeneralizedTime();
     }
 
-    private static DateTimeOffset? ReadX509TimeOpt(ref AsnValueReader reader)
+    private static DateTimeOffset? ReadX509TimeOpt(ref AsnReader reader)
     {
         if (reader.PeekTag().HasSameClassAndValue(Asn1Tag.UtcTime))
         {
--- tests/opencertserver.ca.tests/CrlTests.cs
+++ tests/opencertserver.ca.tests/CrlTests.cs
@@ -10,25 +10,84 @@ public class CrlTests
     [Fact]
     public void CanReadCrl()
     {
-        using var ecdsa = ECDsa.Create();
-        var certRequest = new CertificateRequest(
-            "CN=Test CA,C=US",
-            ecdsa,
-            HashAlgorithmName.SHA256);
-        certRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, false, 0, true));
-        var certificate = certRequest.CreateSelfSigned(
-            DateTimeOffset.UtcNow.AddDays(-1),
-            DateTimeOffset.UtcNow.AddYears(1));
+        var hashAlgorithmName = HashAlgorithmName.SHA256;
+        var certificate = GetEcDsaCertificate(hashAlgorithmName);
+        var crl = GetCrl(certificate, hashAlgorithmName);
+        var loadedCrl = CertificateRevocationList.Load(crl, certificate.GetECDsaPublicKey()!);
+
+        Assert.Equal("C=US, CN=Test CA", loadedCrl.Issuer.Name);
+        Assert.Single(loadedCrl.RevokedCertificates);
+    }
+
+    [Fact]
+    public void CanReadEcdsaCrlSignature()
+    {
+        var certificate = GetEcDsaCertificate(HashAlgorithmName.SHA256);
+        var crl = GetCrl(certificate, HashAlgorithmName.SHA256);
+
+        Assert.True(CertificateRevocationList.VerifyCrlSignature(crl, certificate.GetECDsaPublicKey()!));
+    }
+
+    [Fact]
+    public void CanReadRsaCrlSignature()
+    {
+        var certificate = GetRsaCertificate(HashAlgorithmName.SHA256);
+        var crl = GetCrl(certificate, HashAlgorithmName.SHA256);
+
+        Assert.True(CertificateRevocationList.VerifyCrlSignature(crl, certificate.GetRSAPublicKey()!));
+    }
+
+    private static byte[] GetCrl(X509Certificate2 certificate, HashAlgorithmName hashAlgorithmName)
+    {
         var crlBuilder = new CertificateRevocationListBuilder();
         crlBuilder.AddEntry("1"u8, DateTimeOffset.UtcNow, X509RevocationReason.KeyCompromise);
         var crl = crlBuilder.Build(
             certificate,
             1,
             DateTimeOffset.UtcNow.AddDays(1),
-            HashAlgorithmName.SHA256);
-        var loadedCrl = CertificateRevocationList.Load(crl);
+            hashAlgorithmName,
+            RSASignaturePadding.Pss);
+        return crl;
+    }
 
-        Assert.Equal("C=US, CN=Test CA", loadedCrl.Issuer.Name);
-        Assert.Single(loadedCrl.RevokedCertificates);
+    private static X509Certificate2 GetEcDsaCertificate(HashAlgorithmName hashAlgorithmName)
+    {
+        using var ecdsa = ECDsa.Create();
+        var certRequest = new CertificateRequest(
+            "CN=Test CA,C=US",
+            ecdsa,
+            hashAlgorithmName);
+        certRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, false, 0, true));
+        certRequest.CertificateExtensions.Add(new X509KeyUsageExtension(
+            X509KeyUsageFlags.KeyCertSign | X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.CrlSign, false));
+        certRequest.CertificateExtensions.Add(
+            CertificateRevocationListBuilder.BuildCrlDistributionPointExtension([
+                "http://crl.testca.local/testca.crl"
+            ]));
+        var certificate = certRequest.CreateSelfSigned(
+            DateTimeOffset.UtcNow.AddDays(-1),
+            DateTimeOffset.UtcNow.AddYears(1));
+        return certificate;
+    }
+
+    private static X509Certificate2 GetRsaCertificate(HashAlgorithmName hashAlgorithmName)
+    {
+        using var rsa = RSA.Create();
+        var certRequest = new CertificateRequest(
+            "CN=Test CA,C=US",
+            rsa,
+            hashAlgorithmName,
+            RSASignaturePadding.Pss);
+        certRequest.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, false, 0, true));
+        certRequest.CertificateExtensions.Add(new X509KeyUsageExtension(
+            X509KeyUsageFlags.KeyCertSign | X509KeyUsageFlags.DigitalSignature | X509KeyUsageFlags.CrlSign, false));
+        certRequest.CertificateExtensions.Add(
+            CertificateRevocationListBuilder.BuildCrlDistributionPointExtension([
+                "http://crl.testca.local/testca.crl"
+            ]));
+        var certificate = certRequest.CreateSelfSigned(
+            DateTimeOffset.UtcNow.AddDays(-1),
+            DateTimeOffset.UtcNow.AddYears(1));
+        return certificate;
     }
 }
