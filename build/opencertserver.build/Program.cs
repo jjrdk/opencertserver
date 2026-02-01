32b876f0bb3643a3f7e9753fe8d0dc36ad07b76a
Jacob Reimers <jacob@reimers.dk>
2026-02-01 18:20:05 +0100

    Revert GHA to simple solution build

--- build/opencertserver.build/Program.cs
+++ build/opencertserver.build/Program.cs
@@ -8,7 +8,7 @@ public static class Program
     public static int Main(string[] args)
     {
         return new CakeHost()
-            .InstallTool(new Uri("nuget:?package=GitVersion.Tool&version=5.12.0"))
+            .InstallTool(new Uri("nuget:?package=GitVersion.Tool&version=6.5.1"))
             .UseContext<BuildContext>()
             .Run(args);
     }
