720d146447ed1854f72cd5c5d17ddaa436910030
Jacob Reimers <jacob@reimers.dk>
2026-09-13 13:55:13 +0200

    Fix padding for cross signed certificates.

--- src/opencertserver.ca.utils/Ca/CaProfile.cs
+++ src/opencertserver.ca.utils/Ca/CaProfile.cs
@@ -213,7 +213,7 @@ public record CaProfile : IDisposable
 
         var signatureGenerator = issuerPrivateKey switch
         {
-            RSA rsa => X509SignatureGenerator.CreateForRSA(rsa, RSASignaturePadding.Pss),
+            RSA rsa => X509SignatureGenerator.CreateForRSA(rsa, RSASignaturePadding.Pkcs1),
             ECDsa ecdsa => X509SignatureGenerator.CreateForECDsa(ecdsa),
             _ => throw new NotSupportedException()
         };
--- src/opencertserver.ca/CertificateAuthority.cs
+++ src/opencertserver.ca/CertificateAuthority.cs
@@ -178,7 +178,7 @@ public sealed partial class CertificateAuthority : ICertificateAuthority
         var profilePrivateKey = profile.PrivateKey;
         var x509SignatureGenerator = profilePrivateKey switch
         {
-            RSA rsa => X509SignatureGenerator.CreateForRSA(rsa, RSASignaturePadding.Pss),
+            RSA rsa => X509SignatureGenerator.CreateForRSA(rsa, RSASignaturePadding.Pkcs1),
             ECDsa ecdsa => X509SignatureGenerator.CreateForECDsa(ecdsa),
             _ => throw new NotSupportedException()
         };
