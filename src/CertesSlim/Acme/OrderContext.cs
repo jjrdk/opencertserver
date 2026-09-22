c4427e3812f9f5dfa32b27af48cbe505a987e899
Jacob Reimers <jacob@reimers.dk>
2026-09-22 11:12:56 +0200

    Fix loop

--- src/CertesSlim/Acme/OrderContext.cs
+++ src/CertesSlim/Acme/OrderContext.cs
@@ -42,18 +42,9 @@ internal class OrderContext : EntityContext<Order>, IOrderContext
     {
         var order = await Resource().ConfigureAwait(false);
         var payload = new Order.OrderPayload { Csr = csr.ToBase64String() };
-        while (true)
-        {
-            var resp = await Context.HttpClient.Post<Order, Order.OrderPayload>(Context, order.Finalize!, payload, false)
-                .ConfigureAwait(false);
-            if (resp.Resource.Status == OrderStatus.Processing)
-            {
-                await Task.Delay(TimeSpan.FromSeconds(Math.Max(2, resp.RetryAfter))).ConfigureAwait(false);
-                continue;
-            }
-
-            return resp.Resource;
-        }
+        var resp = await Context.HttpClient.Post<Order, Order.OrderPayload>(Context, order.Finalize!, payload)
+             .ConfigureAwait(false);
+        return resp.Resource;
     }
 
     /// <summary>
